4da6431803e08d9f458fe3c5233448ad86719aa0
Yev Nosenko <yevhenii2208@gmail.com>
2026-05-01 13:26:36 -0400

    Began implementing the hand evl system. Nowhere near finished but I set up all the methods I would wnat (maybe?) and have started working on a wayto recognise flushes.

--- test.gd
+++ test.gd
@@ -1,11 +1,16 @@
 extends Node2D
 var card_scene: PackedScene = preload("res://card.tscn")
+var suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 var dealer = []
 var player = []
 var middle = []
 var hand = []
 
 func _ready() -> void:
+	setup()
+	check_hand(player)
+		
+func setup() -> void:
 	var card_pos = $"Player cards".get_children()
 	
 	for i in 2:
@@ -30,7 +35,32 @@ func _ready() -> void:
 		$"The Middle".add_child(card)
 		var suit_to_value = [card.get_suit(), card.get_value() + 1]
 		middle.append(suit_to_value)
-		
-	print(dealer)
-	print(middle)
-	print(player)
+
+func check_hand(h) -> void: # h is the hand of the player you want to check
+	for i in middle:
+		i[0] = "Hearts"
+	hand = [h, middle]
+	var flush = is_flush(h)
+	if flush:
+		print("Has flush")
+
+func is_flush(h) -> bool:
+	var player_has_flush = false
+	var same_suit = 0
+	for i in suits:
+		var suit_for_flush = i
+		for x in hand:
+			if i == x[0]:
+				same_suit += 1
+		for x in h:
+			if String(x[0]) == suit_for_flush:
+				player_has_flush = true
+		if same_suit >= 5 and player_has_flush:
+			return true
+	return false
+	
+func is_straight() -> bool:
+	return false
+	
+func is_any_pair() -> bool:
+	return false
